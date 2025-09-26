89d54925730c6da228d191a5ef972d238eec2012
GerardoRodriguez19 <gerardo.rodriguez.d@hotmail.com>
2025-09-25 21:51:43 -0600

    Trabajando en carrusel...

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { ThemeProvider } from '@material-tailwind/react'
 
 const root = createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
