7404e7ab98c573eb0c6e31410a8878ad3cb621ae
GerardoRodriguez19 <gerardo.rodriguez.d@hotmail.com>
2025-09-30 21:33:46 -0600

    Post 2 opciones agregadas (1 comentada)

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -6,11 +6,16 @@ const Post = (props) => {
         <div className="flex flex-col p-3 bg-rosa rounded-md mb-3 text-justify">
             <div className="relative">
                 <div className="relative z-10 text-slate-800 p-3">
-                    <h2 className="text-xl mb-10 font-semibold">{titulo}</h2>
+                    <h2 className="text-xl mb-15 font-semibold">{titulo}</h2>
                     <p className="text-slate-950">{descripcion}</p>
                 </div>
-                <img className="absolute left-0 top-0 w-full h-full object-cover opacity-60 rounded-md" src={img} alt="img" />
+                <img className="absolute left-0 top-0 w-full h-full object-cover opacity-35 rounded-md" src={img} alt="img" />
             </div>
+            {/* <div>
+                <h2 className="text-xl mb-3 font-semibold">{titulo}</h2>
+                <img className="w-full h-full object-cover rounded-md mb-6" src={img} alt="img" />
+                <p className="text-slate-950">{descripcion}</p>
+            </div> */}
         </div>
     )
 }
