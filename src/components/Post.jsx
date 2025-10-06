b7b05547bacff80c39915bd6374d148ab3c3486d
GerardoRodriguez19 <gerardo.rodriguez.d@hotmail.com>
2025-10-05 21:37:46 -0600

    - Post arreglado - Fuentes agregadas

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,11 +3,13 @@ const Post = (props) => {
     const { titulo, descripcion, img } = props
 
     return (
-        <div className="flex flex-col p-3 bg-rosa rounded-md mb-3 text-justify">
+        <div className="flex flex-col p-3 bg-rosa rounded-md mb-3">
             <div className="relative">
                 <div className="relative z-10 text-slate-800 p-3">
                     <h2 className="text-xl mb-15 font-semibold">{titulo}</h2>
-                    <p className="text-slate-950">{descripcion}</p>
+                    <div className="flex">
+                        <p className="text-slate-950 text-justify w-1/2">{descripcion}</p>
+                    </div>
                 </div>
                 <img className="absolute left-0 top-0 w-full h-full object-cover opacity-35 rounded-md" src={img} alt="img" />
             </div>
