dcce29b51a1ba3534b6908098b6928f2b9f3fcf9
GerardoRodriguez19 <gerardo.rodriguez.d@hotmail.com>
2025-10-21 22:16:01 -0600

    Metodo de pago agregad (pruebas)

--- src/components/Pago.jsx
+++ src/components/Pago.jsx
@@ -1,20 +1,34 @@
-import { loadStripe } from '@stripe/stripe-js'
-import { Elements, CardElement } from '@stripe/react-stripe-js'
-
-const stripePromise = loadStripe("pk_test_51SFoQw3GSpfJnRuWkIrrxzXxUOHpNuobzO3lw3g9IPK77JpH9iAD47MfOontLl5Lvh0f6uNTJPVLDuiywHnURhBm00tliuVAnr")
-
-const CheckoutForm = () => {
-    return <form>
-        <CardElement />
-    </form>
-}
+import { useStripe, useElements, CardNumberElement, CardExpiryElement } from "@stripe/react-stripe-js"
+import { useState } from "react"
 
 const Pago = () => {
 
+    const stripe = useStripe()
+    const elements = useElements()
+    const [ nombre, setNombre ] = useState('')
+    const [ codigoPostal, setCodigoPostal ] = useState('')
+
     return (
-        <Elements stripe={stripePromise}>
-            <CheckoutForm />
-        </Elements>
+        <div>
+            <div className="relative px-4 max-w-lg mx-auto">
+                <div className="bg-white px-8 py-6 rounded-lg shadow-lg">
+                    <form className="space-y-4">
+                        <div>
+                            <label htmlFor="card-nr" className="block text-sm font-medium mb-1">
+                                Número de tarjeta
+                            </label>
+                            <CardNumberElement id="card-nr" className="text-sm text-gray-700 bg-white border rounded px-3 py-2 border-gray-300" />
+                        </div>
+                        <div>
+                            <label htmlFor="card-exp" className="block text-sm font-medium mb-1">
+                                Fecha de expiración
+                            </label>
+                            <CardExpiryElement id="card-exp" className="text-sm text-gray-700 bg-white border rounded px-3 py-2 border-gray-300" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/pages/Nosotros.jsx
+++ src/pages/Nosotros.jsx
@@ -1,12 +1,15 @@
+import { loadStripe } from '@stripe/stripe-js'
+import { Elements } from '@stripe/react-stripe-js'
 import Pago from '../components/Pago'
+
+const stripe = loadStripe("pk_test_51SFoQw3GSpfJnRuWkIrrxzXxUOHpNuobzO3lw3g9IPK77JpH9iAD47MfOontLl5Lvh0f6uNTJPVLDuiywHnURhBm00tliuVAnr")
 
 const Nosotros = () => {
 
     return (
-        <div className="max-w-7xl mx-auto px-4">
-            <Pago></Pago>
-            Nosotros
-        </div>
+        <Elements stripe={stripe}>
+            <Pago />
+        </Elements>
     )
 }
 
